39f891cf3cdb7d2ca8d269678352d7d9137944b4
Francesco Leoni <leoni.francesco98@gmail.com>
2024-06-28 09:59:22 +0200

    Fix lottie version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/airbnb/lottie-ios", from: "4.0.0"),
+			.package(url: "https://github.com/airbnb/lottie-ios", .exact("4.4.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
